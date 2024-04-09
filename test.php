cb3d4d0843f5e36be745a96c206e2ce5a680c7ef
Meagan Went <106477587+MeaganWent@users.noreply.github.com>
2024-04-09 11:51:43 -0400

    Update test.php

--- test.php
+++ test.php
@@ -1,36 +1,36 @@
-<?php   										// Opening PHP tag
+// <?php   										// Opening PHP tag
 	
-	// Include the database connection script
-	require 'includes/database-connection.php';
+// 	// Include the database connection script
+// 	require 'includes/database-connection.php';
 
-	/*
-	 * Retrieve toy information from the database based on the toy ID.
-	 * 
-	 * @param PDO $pdo       An instance of the PDO class.
-	 * @param string $id     The ID of the toy to retrieve.
-	 * @return array|null    An associative array containing the toy information, or null if no toy is found.
-	 */
-	function get_toy(PDO $pdo, string $id) {
+// 	/*
+// 	 * Retrieve toy information from the database based on the toy ID.
+// 	 * 
+// 	 * @param PDO $pdo       An instance of the PDO class.
+// 	 * @param string $id     The ID of the toy to retrieve.
+// 	 * @return array|null    An associative array containing the toy information, or null if no toy is found.
+// 	 */
+// 	function get_toy(PDO $pdo, string $id) {
 
-		// SQL query to retrieve toy information based on the toy ID
-		$sql = "SELECT * 
-			FROM pokemon
-			WHERE PokemonID= :id;";	// :id is a placeholder for value provided later 
-		                               // It's a parameterized query that helps prevent SQL injection attacks and ensures safer interaction with the database.
+// 		// SQL query to retrieve toy information based on the toy ID
+// 		$sql = "SELECT * 
+// 			FROM pokemon
+// 			WHERE PokemonID= :id;";	// :id is a placeholder for value provided later 
+// 		                               // It's a parameterized query that helps prevent SQL injection attacks and ensures safer interaction with the database.
 
 
-		// Execute the SQL query using the pdo function and fetch the result
-		$toy = pdo($pdo, $sql, ['PokemonID' => $id])->fetch();		// Associative array where 'id' is the key and $id is the value. Used to bind the value of $id to the placeholder :id in  SQL query.
+// 		// Execute the SQL query using the pdo function and fetch the result
+// 		$toy = pdo($pdo, $sql, ['PokemonID' => $id])->fetch();		// Associative array where 'id' is the key and $id is the value. Used to bind the value of $id to the placeholder :id in  SQL query.
 
-		// Return the toy information (associative array)
-		return $toy;
-	}
+// 		// Return the toy information (associative array)
+// 		return $toy;
+// 	}
 
-	// Retrieve info about toy with ID '0001' from the db using provided PDO connection
-	$toy1 = get_toy($pdo, '1');
+// 	// Retrieve info about toy with ID '0001' from the db using provided PDO connection
+// 	$toy1 = get_toy($pdo, '1');
 	
 
-// Closing PHP tag  ?> 
+// // Closing PHP tag  ?> 
 
 <!DOCTYPE>
 <html>
