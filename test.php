75a15ef7a18f702c6dee76fcdd4518c318dced78
Meagan Went <106477587+MeaganWent@users.noreply.github.com>
2024-04-09 11:50:53 -0400

    Update test.php

--- test.php
+++ test.php
@@ -15,12 +15,12 @@
 		// SQL query to retrieve toy information based on the toy ID
 		$sql = "SELECT * 
 			FROM pokemon
-			WHERE pokemonID= :id;";	// :id is a placeholder for value provided later 
+			WHERE PokemonID= :id;";	// :id is a placeholder for value provided later 
 		                               // It's a parameterized query that helps prevent SQL injection attacks and ensures safer interaction with the database.
 
 
 		// Execute the SQL query using the pdo function and fetch the result
-		$toy = pdo($pdo, $sql, ['id' => $id])->fetch();		// Associative array where 'id' is the key and $id is the value. Used to bind the value of $id to the placeholder :id in  SQL query.
+		$toy = pdo($pdo, $sql, ['PokemonID' => $id])->fetch();		// Associative array where 'id' is the key and $id is the value. Used to bind the value of $id to the placeholder :id in  SQL query.
 
 		// Return the toy information (associative array)
 		return $toy;
@@ -28,15 +28,6 @@
 
 	// Retrieve info about toy with ID '0001' from the db using provided PDO connection
 	$toy1 = get_toy($pdo, '1');
-	$toy2 = get_toy($pdo, '2');
-	$toy3 = get_toy($pdo, '3');
-	$toy4 = get_toy($pdo, '4');
-	$toy5 = get_toy($pdo, '5');
-	$toy6 = get_toy($pdo, '6');
-	$toy7 = get_toy($pdo, '7');
-	$toy8 = get_toy($pdo, '8');
-	$toy9 = get_toy($pdo, '9');
-	$toy10 = get_toy($pdo, '10');
 	
 
 // Closing PHP tag  ?> 
