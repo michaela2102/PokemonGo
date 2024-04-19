acd330d4f138682cba3e65c3b75d8be439aaee59
Sarah Dzwil <sarahdzwil@Sarahs-MacBook-Air.local>
2024-04-19 11:38:56 -0400

    Created home page 1st draft. Made blank files for login and create account pages

---
+++ home.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pokémon Home Page</title>
+    <style>
+        /* Basic CSS for styling */
+        body {
+            font-family: Arial, sans-serif;
+        }
+        h1 {
+            text-align: center;
+        }
+        ul {
+            list-style-type: none;
+            padding: 0;
+        }
+        li {
+            margin-bottom: 10px;
+            text-align: center;
+        }
+        a {
+            text-align: center;
+        }
+        h2 {
+            text-align: center;
+        }
+        div {
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <h1>Welcome to our PokémonGo Web Application!</h1>
+
+    <center>Here you can log in to your account, or create an account if you don't already have one.</center><br>
+    <center>Or, view the leaderboard of PokémonGo players and cheer on your friends!</center><br>
+    <center>Once you log in, you can view your current Pokémon.</center><br>
+    <br>
+    <li><a href="login.php">Log in to your account</a></li> <br>
+    <li><a href="create_account.php">Create an account</a></li> <br>
+    <li><a href="leaderboard.php">View the leaderboard</a></li> <br>
+    
+    <center>Web Application created and designed by Group 5</center>
+
+    
+</body>
+</html>
