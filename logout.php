e689a0cf2c2389a3b0cf89c9475e844835dd5788
Meagan Went <106477587+MeaganWent@users.noreply.github.com>
2024-04-23 03:58:21 +0000

    thinking hard

--- logout.php
+++ logout.php
@@ -8,6 +8,11 @@ $_SESSION = array();
 // Destroy the session
 session_destroy();
 
+if (isset($_COOKIE['auth_token'])) {
+    // Set the expiration time of the cookie to the past
+    setcookie('auth_token', '', time() - 3600, '/');
+}
+
 // Redirect to the login page or any other appropriate page
 header("Location: home.php");
 exit();
