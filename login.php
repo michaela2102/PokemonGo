7af26627c3769f99bea75fa3127eafb796bd2eb3
Meagan Went <106477587+MeaganWent@users.noreply.github.com>
2024-04-23 15:14:04 +0000

    loging

--- login.php
+++ login.php
@@ -50,7 +50,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Cosmic Horoscopes</title>
+  <title>Log-in</title>
 </head>
 
 <body>
