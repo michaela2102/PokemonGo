684e5cf973cdf5337e7f2120c3acc7d42da1980b
Meagan Went <106477587+MeaganWent@users.noreply.github.com>
2024-04-24 12:11:39 +0000

    looking better

--- home.php
+++ home.php
@@ -5,6 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pokémon Home Page</title>
     <style>
+        li {
+            margin-bottom: 10px;
+            text-align: center;
+        }
         body {
             background: #98C8DE;
             color: #333;
@@ -95,23 +99,10 @@
             border-radius: 5px;
             cursor: pointer;
         }
-
-        /* Responsive styles */
-        @media (max-width: 768px) {
-            .header-left,
-            .header-right {
-                float: none;
-                text-align: center;
-            }
-
-            .sidebar {
-                height: auto;
-            }
-
-            .col-md-3.sidebar,
-            .col-md-9.content {
-                width: 100%;
-            }
+        .login-icon {
+            position: absolute;
+            top: 10px; /* Adjust as needed */
+            left: 10px; /* Adjust as needed */
         }
     </style>
 </head>
@@ -122,13 +113,12 @@
     <center>Or, view the leaderboard of PokémonGo players and cheer on your friends!</center><br>
     <center>Once you log in, you can view your current Pokémon.</center><br>
     <br>
-    <a href="login.php">
+    <a href="login.php" class="login-icon">
         <img src="images/login.png" alt="Login Icon" width= "20%" height="20%">
     </a><br>
-    <a href="create_account.php">Create an account</a><br>
-    <a href="pokedex.php">View the Pokédex</a></li> <br>
-    <a href="leaderboard.php">View the leaderboard</a><br>
-    <a href="trading_page.php">View the Pokémon trading page</a> <br>
+    <li><a href="create_account.php">New user? Create an account</a></li> <br>
+    <li><a href="leaderboard.php">View the leaderboard</a></li> <br>
+    <li><a href="trading_page.php">View the pokemon trading page</a></li> <br>
     
     <center>Pokémon Go Community Web Application created and designed by Group 5</center>
 
