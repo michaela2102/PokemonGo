5e30fb36efbeb7f42df2759adbe5f3b396007cc4
Meagan Went <106477587+MeaganWent@users.noreply.github.com>
2024-04-24 10:27:53 -0400

    Update leaderboard.php

--- leaderboard.php
+++ leaderboard.php
@@ -84,8 +84,6 @@
 		<div class="header-right">
 		    <ul>
                 <li><a href="pokedex.php">Pokédex</a></li>
-		    	<li><a href="hasPokemon.php">My Collection</a></li>
-				<li><a href="logout.php">Log Out</a> </li>
 		    </ul>
 		</div>
 	</header>
