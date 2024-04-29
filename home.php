94c265a5b8d2324c914d3e06d4a7f7ae7ac41791
Michaela Healy <michaela_healy@uri.edu>
2024-04-29 15:39:53 -0400

    added pix

--- home.php
+++ home.php
@@ -94,9 +94,9 @@
     <center>Here you can log in to your account, or create an account if you don't already have one.</center><br>
     <center>Or, view the leaderboard of PokémonGo players and cheer on your friends!</center><br>
     <center>Once you log in, you can view your current Pokémon.</center><br>
-    <li><a href="create_account.php">New user? Create an account</a></li> <br>
-    <li><a href="leaderboard.php">View the leaderboard</a></li> <br>
-    <li><a href="trading_page.php">View the pokemon trading page</a></li> <br>
+    <li><a href="create_account.php">New user? Create an account</a><img src="images/pikachu.png" alt="Pikachu Icon" width= "20" height="20"></li><br>
+    <li><a href="leaderboard.php">View the leaderboard</a><img src="images/ditto.png" alt="Ditto Icon" width= "20" height="20"></li><br>
+    <li><a href="trading_page.php">View the pokemon trading page</a><img src="images/charmander.png" alt="Charmander Icon" width= "20" height="20"></li><br>
     
     <center>Pokémon Go Community Web Application created and designed by Group 5</center>
     
